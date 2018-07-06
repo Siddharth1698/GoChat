2403d0b9ce7d3eac6dec98d4d12a46128459bbba
Siddharth M <siddharth.m98@gmail.com>
2018-07-06 14:45:05 +0530

    Creating new Account

---
+++ app/src/main/java/com/siddharthm/gochat/RegisterActivity.java
@@ -0,0 +1,66 @@
+package com.siddharthm.gochat;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateBtn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        mDisplayName = (TextInputLayout)findViewById(R.id.regDisplayName);
+        mEmail = (TextInputLayout)findViewById(R.id.regEmail);
+        mPassword = (TextInputLayout)findViewById(R.id.regPassword);
+        mCreateBtn = (Button)findViewById(R.id.reg_create_btn);
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String displayName = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+                register_new_user(displayName,email,password);
+
+            }
+        });
+    }
+
+    private void register_new_user(String displayName, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent main_intent = new Intent(RegisterActivity.this,MainActivity.class);
+                            startActivity(main_intent);
+                        } else {
+
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
