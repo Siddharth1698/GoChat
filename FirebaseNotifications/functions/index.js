737829b2e1a942e36501a06480dbf0e7bb6945cb
Siddharth M <siddharth.m98@gmail.com>
2018-07-17 23:40:21 +0530

    Node js push notification with firebase

--- FirebaseNotifications/functions/index.js
+++ FirebaseNotifications/functions/index.js
@@ -2,11 +2,28 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
-exports.sendNotification = functions.database
-.ref('/notifications/{user_id}/{notification_id}').onWrite(event => {
 
-	const user_id = event.params.user_id;
-	const notifications = event.params.notifications;
-});
+/*
+ * 'OnWrite' works as 'addValueEventListener' for android. It will fire the function
+ * everytime there is some item added, removed or changed from the provided 'database.ref'
+ * 'sendNotification' is the name of the function, which can be changed according to
+ * your requirement
+ */
+
+exports.sendNotification = functions.database.ref('/notifications/{user_id}/{notification_id}').onWrite((data, context) => {
+
+
+  /*
+   * You can store values as variables from the 'database.ref'
+   * Just like here, I've done for 'user_id' and 'notification'
+   */
+
+  const user_id = context.params.user_id;
+  const notification_id = context.params.notification_id;
+
+  
+ 
+
+  });﻿
